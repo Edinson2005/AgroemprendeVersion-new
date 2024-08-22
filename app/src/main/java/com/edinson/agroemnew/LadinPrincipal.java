04090c63e4eeca61cd19d0add5506b9d0a2b857a
vargas023 <alejandravargasb1@gmail.com>
2024-08-22 15:17:08 -0500

    Se agrego animacion para el logo del inicio

--- app/src/main/java/com/edinson/agroemnew/LadinPrincipal.java
+++ app/src/main/java/com/edinson/agroemnew/LadinPrincipal.java
@@ -4,7 +4,12 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
@@ -13,6 +18,9 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class LadinPrincipal extends AppCompatActivity {
 
+    private ProgressBar progressBar;
+    private ImageView imageView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,43 +28,37 @@ public class LadinPrincipal extends AppCompatActivity {
 
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
-            setContentView(R.layout.activity_ladin_principal);
         }
+        setContentView(R.layout.activity_ladin_principal);
 
-            // Manejar las inserciones de ventana para la vista principal
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-                return insets;
-            });
+        imageView = findViewById(R.id.imageView3);
 
+        // Cargar y aplicar animación
+        Animation slideInAnimation = AnimationUtils.loadAnimation(this, R.anim.slide_in);
+        imageView.startAnimation(slideInAnimation);
+
+        // Manejar las inserciones de ventana para la vista principal
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
 
         SharedPreferences sharedPreferences = getSharedPreferences("MyApp", MODE_PRIVATE);
         String token = sharedPreferences.getString("UserToken", null);
 
         if (token != null) {
             // Token encontrado, redirige a la pantalla principal
-            Intent intent = new Intent(LadinPrincipal.this, MainActivity.class); // Cambia MainActivity por tu actividad principal
+            Intent intent = new Intent(LadinPrincipal.this, MainActivity.class);
             startActivity(intent);
             finish(); // Cierra esta actividad para que el usuario no pueda volver a ella con el botón de retroceso
         } else {
             // Token no encontrado, muestra la pantalla de inicio de sesión
-            setContentView(R.layout.activity_ladin_principal);
-
-            // Manejar las inserciones de ventana para la vista principal
-            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-                return insets;
-            });
-
-            // Configurar el botón para iniciar sesión
             Button loginButton = findViewById(R.id.button);
             loginButton.setOnClickListener(v -> {
                 Intent intent = new Intent(LadinPrincipal.this, Login.class);
                 startActivity(intent);
             });
         }
-
-        }
     }
+}
