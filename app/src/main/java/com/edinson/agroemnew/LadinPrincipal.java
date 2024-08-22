3c9cd388330cd68d23a6199619c485ab9167dfd7
vargas023 <alejandravargasb1@gmail.com>
2024-08-22 14:32:35 -0500

    Se mantiene el inicio de sesion asi se salga de la aplicacion mientras dure el token

--- app/src/main/java/com/edinson/agroemnew/LadinPrincipal.java
+++ app/src/main/java/com/edinson/agroemnew/LadinPrincipal.java
@@ -1,6 +1,7 @@
 package com.edinson.agroemnew;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -17,7 +18,6 @@ public class LadinPrincipal extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
 
-
         if (getSupportActionBar() != null) {
             getSupportActionBar().hide();
             setContentView(R.layout.activity_ladin_principal);
@@ -30,14 +30,33 @@ public class LadinPrincipal extends AppCompatActivity {
                 return insets;
             });
 
+
+        SharedPreferences sharedPreferences = getSharedPreferences("MyApp", MODE_PRIVATE);
+        String token = sharedPreferences.getString("UserToken", null);
+
+        if (token != null) {
+            // Token encontrado, redirige a la pantalla principal
+            Intent intent = new Intent(LadinPrincipal.this, MainActivity.class); // Cambia MainActivity por tu actividad principal
+            startActivity(intent);
+            finish(); // Cierra esta actividad para que el usuario no pueda volver a ella con el botón de retroceso
+        } else {
+            // Token no encontrado, muestra la pantalla de inicio de sesión
+            setContentView(R.layout.activity_ladin_principal);
+
+            // Manejar las inserciones de ventana para la vista principal
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+                Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+                return insets;
+            });
+
             // Configurar el botón para iniciar sesión
             Button loginButton = findViewById(R.id.button);
-            loginButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(LadinPrincipal.this,   Login.class);
-                    startActivity(intent);
-                }
+            loginButton.setOnClickListener(v -> {
+                Intent intent = new Intent(LadinPrincipal.this, Login.class);
+                startActivity(intent);
             });
+        }
+
         }
     }
