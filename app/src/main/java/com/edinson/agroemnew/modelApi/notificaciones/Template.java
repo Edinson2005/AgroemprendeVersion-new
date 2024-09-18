f31575b0587d9fa6d21a461b1d424a5bafc06fd4
vargas023 <alejandravargasb1@gmail.com>
2024-09-18 15:14:59 -0500

    pequeños cambios

---
+++ app/src/main/java/com/edinson/agroemnew/modelApi/notificaciones/Template.java
@@ -0,0 +1,13 @@
+package com.edinson.agroemnew.modelApi.notificaciones;
+
+public class Template {
+    private String titulo;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+}
